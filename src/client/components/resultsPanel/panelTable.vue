74d969bd7110390b946b7f04a998630280ea2a64
Grey Delamar <grey.delamar@gmail.com>
2020-07-13 19:12:16 -0400

    Show Errors & Rows affected in results panel

--- src/client/components/resultsPanel/panelTable.vue
+++ src/client/components/resultsPanel/panelTable.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div v-for="(result, idx) in currentResults(openEditor.guiID)" :key="openEditor.guiID+'-table-'+idx">
-      <v-data-table :ref="openEditor.guiID+'-table-'+idx" :height="tableHeight / currentResults(openEditor.guiID).length" :fixed-header="true" :loading="openEditor.resultsPanelLoading" :headers="getEditorTabResultKeys(openEditor.guiID, idx)" :items="getEditorTabResults(openEditor.guiID, idx)" :items-per-page="15" :footer-props="{'items-per-page-options':[15, 30, 50, 75, 100, -1]}" dense :class="{'elevation-1': true, 'showing-scroll': showingScroll(openEditor.guiID+'-table-'+idx)}"></v-data-table>
+      <v-data-table v-if="Array.isArray(result)" :ref="openEditor.guiID+'-table-'+idx" :height="tableHeight / currentResults(openEditor.guiID).length" :fixed-header="true" :loading="openEditor.resultsPanelLoading" :headers="getEditorTabResultKeys(openEditor.guiID, idx)" :items="getEditorTabResults(openEditor.guiID, idx)" :items-per-page="15" :footer-props="{'items-per-page-options':[15, 30, 50, 75, 100, -1]}" dense :class="{'elevation-1': true, 'showing-scroll': showingScroll(openEditor.guiID+'-table-'+idx)}"></v-data-table>
+      <div v-else class="pa-3" style="color: #fff">
+        <p>{{ result.sqlMessage }}</p>
+        <p v-show="result.affectedRows">Affected Rows: {{ result.affectedRows }}</p>
+        <p v-show="result.localRanAt">Ran At: {{ result.localRanAt }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -22,7 +27,14 @@ export default class ResultsPanelTable extends Vue {
 
   currentResults (editorGuiID: string) {
     let data = this.editorTabsResults[editorGuiID] || {}
-    return data.recordsets || []
+    let results = !data.error ? data.recordsets : [data]
+
+    if (results[0].affectedRows == 0) {
+      results[0].localRanAt = data.localRanAt
+      return results
+    }
+
+    return results
   }
 
   showingScroll (ref: string) {
@@ -30,9 +42,9 @@ export default class ResultsPanelTable extends Vue {
       const r = <any>this.$refs[ref];
       const target = Array.isArray(r) ? r[0] : r;
 
-      if (target.$el.getElementsByTagName('table')[0].offsetWidth > target.$el.offsetWidth) {
+      if (target && target.$el.getElementsByTagName && target.$el.getElementsByTagName('table')[0].offsetWidth > target.$el.offsetWidth) {
         target.$el.getElementsByClassName('v-data-footer')[0].style.bottom = "9px"
-      } else {
+      } else if (target && target.$el.getElementsByTagName) {
         target.$el.getElementsByClassName('v-data-footer')[0].style.bottom = "0px"
       }
     })
@@ -42,8 +54,9 @@ export default class ResultsPanelTable extends Vue {
 
   get getEditorTabResultKeys() {
     return (editorGuiID: string, idx: number) => {
-      if(this.editorTabsResults[editorGuiID]) {
-        return Object.keys(this.editorTabsResults[editorGuiID].recordsets[idx][0]).map(d => ({ text: d, value: d }))
+      let tabResults = this.editorTabsResults[editorGuiID]
+      if (tabResults && !tabResults.error && tabResults.recordsets) {
+        return Object.keys(tabResults.recordsets[idx][0]).map(d => ({ text: d, value: d }))
       }
 
       return []
@@ -52,9 +65,10 @@ export default class ResultsPanelTable extends Vue {
 
   get getEditorTabResults() {
     return (editorGuiID: string, idx: number) => {
-      if(this.editorTabsResults[editorGuiID]) {
-        let results = this.editorTabsResults[editorGuiID].recordsets[idx]
-        return results
+      let tabResults = this.editorTabsResults[editorGuiID]
+
+      if (tabResults && !tabResults.error && tabResults.recordsets) {
+        return tabResults.recordsets[idx]
       }
 
       return []
